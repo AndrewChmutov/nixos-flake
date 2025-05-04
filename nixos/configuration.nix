982c18437c9c0f274a5669fd796ae175d9f7316b
Andrew Chmutov <ulman.andre@gmail.com>
2025-05-04 23:39:32 +0200

    feat: cli brightness control

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -54,6 +54,10 @@
       touchpad.naturalScrolling = true;
     };
 
+    udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="intel_backlight", MODE="0666", RUN+="${pkgs.coreutils}/bin/chmod a+w /sys/class/backlight/%k/brightness"
+    '';
+
     openssh.enable = true;
   };
 
